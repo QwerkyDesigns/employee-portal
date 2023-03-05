d1f60e87fca499462a42d543d2fb7f2bdffa0dd5
Adam Beck <adamb3189@gmail.com>
2023-03-05 20:36:24 +1100

    feat: refactor and comments

--- src/lib/controllers/base/AuthenticatedBaseController.ts
+++ src/lib/controllers/base/AuthenticatedBaseController.ts
@@ -1,18 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Controller } from "nextjs-backend-helpers";
-import prismaClient from "../../client/prisma";
 import UnAuthenticatedError from "../../errors/bad-request/UnAuthenticatedError";
-import { PrismaClient } from "@prisma/client";
 import { getSession } from "next-auth/react";
 import { rescue_Error, rescue_UnAuthenticatedError } from "./RescueSupport";
+import { prisma } from "@/lib/client/prisma";
 
 export class AuthenticatedBaseController extends Controller {
-    public readonly db: PrismaClient;
+    public readonly db: typeof prisma;
 
     constructor() {
         super();
 
-        this.db = prismaClient;
+        this.db = prisma;
         this.before(async (req: NextApiRequest, res: NextApiResponse) => {
             const session = await getSession({ req });
             if (!session) {
