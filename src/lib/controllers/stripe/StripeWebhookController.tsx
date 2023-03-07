db7be01e12fbc76456e925b8fb7d9492c0c10609
Paul Gradie <paul.e.gradie@gmail.com>
2023-03-07 17:42:31 +1100

    update docker compose

--- src/lib/controllers/stripe/StripeWebhookController.tsx
+++ src/lib/controllers/stripe/StripeWebhookController.tsx
@@ -1,22 +1,20 @@
 import stripeBackendClient from "@/lib/client/stripe";
-import env from "@/lib/environment/Environment";
-import { EnvironmentVariable } from "@/lib/environment/EnvironmentVariable";
+
 import StripeSignatureError from "@/lib/errors/application-errors/StripeSignatureError";
-import { handleWebhookEvent  } from "@/lib/services/stripe/stripeWebhookEntryService";
+import { handleWebhookEvent } from "@/lib/services/stripe/stripeWebhookEntryService";
 import { NextApiRequest, NextApiResponse } from "next/types";
 import { errors, error, Logger } from "nextjs-backend-helpers";
 import { Controller } from "nextjs-backend-helpers/controller";
 import { getStripeHeader } from "./headers";
 import { hasStripeHeader } from "./middleware";
-import { StatusCodes } from '@/lib/enums/StatusCodes';
+import { StatusCodes } from "@/lib/enums/StatusCodes";
+import { env } from "../../../env/server.mjs";
 
-const endpointSecret = env.
-    GetStringEnvironmentVarialble(EnvironmentVariable.StripeWebhookSecret);
+const endpointSecret = env.STRIPE_WEBHOOK_SECRET;
 // This is an unauthenticated stripe webhook controller
 // we'll add middleware on to this to reject any request that does not
 // contain the required headers from stripe
 class StripeWebhookController extends Controller {
-
     constructor() {
         super();
 
@@ -24,28 +22,29 @@ class StripeWebhookController extends Controller {
             res.status(400).json(errors([err.constructor.name, err.message]));
         });
 
-        this.before(hasStripeHeader).only('post');
+        // this.before(hasStripeHeader).only("post"); // temp comment
     }
 
     async post(req: NextApiRequest, res: NextApiResponse<StripeWebhookResponse>) {
+        console.log("WE HIT THE STRIPE WEBHOOK ENDPOINT");
+        console.log(req.headers);
+
         const sig = getStripeHeader<string>(req);
 
+        console.log(sig);
         try {
-            const event = stripeBackendClient
-                .webhooks
-                .constructEvent(req.body, sig, endpointSecret);
+            const event = stripeBackendClient.webhooks.constructEvent(req.body, sig, endpointSecret);
             await handleWebhookEvent(event, sig);
         } catch (err: any) {
             const errr = err as Error;
 
             Logger.error({
-                message: 'Webhook Error',
+                message: "Webhook Error",
                 error: errr.message,
-                errorName: errr.name
+                errorName: errr.name,
             });
 
-            res.status(StatusCodes.InvalidRequest)
-                .json(error(`Webhook Error: ${err.message}`));
+            res.status(StatusCodes.InvalidRequest).json(error(`Webhook Error: ${err.message}`));
 
             return;
         }
