53326be8d4af778e3d466544949157f768803f95
Paul Gradie <paul.e.gradie@gmail.com>
2023-03-08 23:20:51 +1100

    fix import

--- src/lib/controllers/stripe/middleware.ts
+++ src/lib/controllers/stripe/middleware.ts
@@ -3,14 +3,13 @@ import StripeSignatureError from "@/lib/errors/application-errors/StripeSignatur
 import { NextApiRequest, NextApiResponse } from "next";
 import { error } from "nextjs-backend-helpers";
 import { getStripeHeader } from "./headers";
-import { StripeWebhookResponse } from "./StripeWebhookController";
 
-export function hasStripeHeader(req: NextApiRequest, res: NextApiResponse<StripeWebhookResponse>, stop: () => void) {
-  const sig = getStripeHeader(req);
+export function hasStripeHeader(req: NextApiRequest, res: NextApiResponse, stop: () => void) {
+    const sig = getStripeHeader(req);
 
-  if (typeof sig !== "string") {
-      stop();
-      res.status(StatusCodes.InvalidRequest).json(error('invalid request'));
-      throw new StripeSignatureError("Errors everywhere!?");
-  }
+    if (typeof sig !== "string") {
+        stop();
+        res.status(StatusCodes.InvalidRequest).json(error("invalid request"));
+        throw new StripeSignatureError("Errors everywhere!?");
+    }
 }
