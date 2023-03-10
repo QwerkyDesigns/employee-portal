c74792a7f805ae97219683d258f062d96b769695
Paul Gradie <paul.e.gradie@gmail.com>
2023-03-10 23:17:41 +1100

    comment auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,39 +5,39 @@ import { prisma } from '@/lib/client/prisma';
 
 export const authOptions: NextAuthOptions = {
     providers: [
-        CredentialsProvider({
-            name: 'Username or Email and Password',
-            credentials: {
-                usernameOrEmail: { label: 'Username', type: 'text', placeholder: 'username or email' },
-                password: { label: 'Password', type: 'password' }
-            },
-            async authorize(credentials, req): Promise<User | null> {
-                let account = await prisma.account.findUnique({
-                    where: {
-                        userName: credentials?.usernameOrEmail
-                    }
-                });
+        // CredentialsProvider({
+        //     name: 'Username or Email and Password',
+        //     credentials: {
+        //         usernameOrEmail: { label: 'Username', type: 'text', placeholder: 'username or email' },
+        //         password: { label: 'Password', type: 'password' }
+        //     },
+        //     async authorize(credentials, req): Promise<User | null> {
+        //         let account = await prisma.account.findUnique({
+        //             where: {
+        //                 userName: credentials?.usernameOrEmail
+        //             }
+        //         });
 
-                if (account === null) {
-                    account = await prisma.account.findUnique({
-                        where: {
-                            email: credentials?.usernameOrEmail
-                        }
-                    });
-                }
-                if (account === null) {
-                    return null;
-                }
+        //         if (account === null) {
+        //             account = await prisma.account.findUnique({
+        //                 where: {
+        //                     email: credentials?.usernameOrEmail
+        //                 }
+        //             });
+        //         }
+        //         if (account === null) {
+        //             return null;
+        //         }
 
-                // TODO: encrypt this with salt. I have prior art for this available - later b4 prod tho
-                if (account?.password !== credentials?.password) {
-                    return null;
-                }
+        //         // TODO: encrypt this with salt. I have prior art for this available - later b4 prod tho
+        //         if (account?.password !== credentials?.password) {
+        //             return null;
+        //         }
 
-                const user: User = { id: account?.id.toString(), email: account?.email, name: account.userName };
-                return user;
-            }
-        }),
+        //         const user: User = { id: account?.id.toString(), email: account?.email, name: account.userName };
+        //         return user;
+        //     }
+        // }),
         GithubProvider({
             clientId: process.env.GITHUB_CLIENT_ID,
             clientSecret: process.env.GITHUB_CLIENT_SECRET
