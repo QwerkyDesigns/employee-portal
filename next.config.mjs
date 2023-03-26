a2823b627d2cf74930f20ccd3f855a99c3d41fc5
Paul Gradie <paul.e.gradie@gmail.com>
2023-03-26 13:07:43 +1100

    uncommend mdx - for build

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ import withMDX from '@next/mdx';
  * @constraint {{import('next').NextConfig}}
  */
 function defineNextConfig(config) {
-    // return withMDX(config);
-    return config;
+    return withMDX(config);
+    // return config;
     // if (process.env.NODE_ENV === 'development') {
     //     return config;
     // }
