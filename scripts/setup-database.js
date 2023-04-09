8d24b4219449556fc80b70564b08d0df1dcb760d
Paul Gradie <paul.e.gradie@gmail.com>
2023-04-09 11:16:52 +1000

    update nextauth and dont setup db - and only git atm

--- scripts/setup-database.js
+++ scripts/setup-database.js
@@ -2,78 +2,10 @@ require('dotenv').config({ path: './scripts/.env.external' });
 const config = require('./../dev.config.js');
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.DATABASE_URL
-        }
-    }
-});
-
-prisma.user
-    .create({
-        data: {
-            name: config.name,
-            email: config.email,
-            password: config.password,
-            sessions: {
-                create: [
-                    // {
-                    //     sessionToken: 'sessiontoken123',
-                    //     expires: new Date('2023-12-31T23:59:59Z')
-                    // }
-                ]
-            },
-            accounts: {
-                create: [
-                    {
-                        stripeCustomerId: config.stripeCustomerId,
-                        type: 'default',
-                        provider: 'password',
-                        providerAccountId: 'account123',
-                        refresh_token: null,
-                        access_token: null,
-                        expires_at: null, // UNIX timestamp for 2022-12-31 00:00:00 UTC
-                        token_type: null,
-                        scope: 'read', // on account verification: 'read write'
-                        id_token: null,
-                        session_state: null
-                    }
-                ]
-            }
-        }
-    })
-    .then((user) => {
-        console.log('Account Data stored in the database.');
-        prisma.account
-            .findFirst({
-                where: {
-                    userId: user.id
-                }
-            })
-            .then((account) => {
-                prisma.usage
-                    .create({
-                        data: {
-                            availableFunds: 100.0,
-                            Account: {
-                                connect: {
-                                    id: account.id
-                                }
-                            }
-                        }
-                    })
-                    .then(() => {
-                        console.log('Usage funds stored in the database.');
-                    })
-                    .catch((error) => {
-                        console.error('Failed to store usage data:', error);
-                    })
-                    .finally(() => {
-                        prisma.$disconnect();
-                    });
-            });
-    })
-    .catch((error) => {
-        console.error('Failed to store account data:', error);
-    });
+// const prisma = new PrismaClient({
+//     datasources: {
+//         db: {
+//             url: process.env.DATABASE_URL
+//         }
+//     }
+// });
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,70 +5,74 @@ import { prisma } from '@/lib/client/prisma';
 import { compareCredentials } from '@/lib/utils/credentials/compareCredentials';
 import { JWTOptions } from 'next-auth/jwt';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { env } from '@/env/server.mjs';
 
 export default NextAuth({
     adapter: PrismaAdapter(prisma),
+    debug: true,
     providers: [
         GithubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
-        CredentialsProvider({
-            name: 'credentials',
-            type: 'credentials',
-            credentials: {
-                emailaddress: { label: 'Email', type: 'email', placeholder: 'Your email' },
-                password: { label: 'Password', type: 'password', placeholder: 'Your Password' }
-            },
-            authorize: async (credentials): Promise<User | null> => {
-                if (credentials === null || credentials === undefined) {
-                    return null;
-                }
-
-                const email = credentials.emailaddress;
-                const password = credentials.password;
-                if (email === null || password === password) {
-                    return null;
-                }
-
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.emailaddress
-                    }
-                });
-                if (user === null || user.password === null) {
-                    return null;
-                }
-
-                const result = compareCredentials(credentials.password, user.password);
-
-                if (!result) {
-                    return null;
-                }
-                return user;
-            }
+            clientId: env.GITHUB_CLIENT_ID,
+            clientSecret: env.GITHUB_CLIENT_SECRET
         })
-    ],
-    callbacks: {
-        jwt: async ({ token, user }) => {
-            if (user) {
-                token.id = user.id;
-            }
-            return token;
-        },
-        session: ({ session, token }) => {
-            // if (token && session.id! !== undefined) {
-            //     session.id = token.id;
-            // }
-            return session;
-        }
-    },
-    secret: 'test',
-    session: {
-        strategy: 'jwt'
-    },
-    jwt: {
-        secret: 'test',
-        encryption: true
-    } as Partial<JWTOptions>
+    ]
 });
+//     CredentialsProvider({
+//         name: 'credentials',
+//         type: 'credentials',
+//         credentials: {
+//             emailaddress: { label: 'Email', type: 'email', placeholder: 'Your email' },
+//             password: { label: 'Password', type: 'password', placeholder: 'Your Password' }
+//         },
+//         authorize: async (credentials): Promise<User | null> => {
+//             if (credentials === null || credentials === undefined) {
+//                 return null;
+//             }
+
+//             const email = credentials.emailaddress;
+//             const password = credentials.password;
+//             if (email === null || password === password) {
+//                 return null;
+//             }
+
+//             const user = await prisma.user.findUnique({
+//                 where: {
+//                     email: credentials.emailaddress
+//                 }
+//             });
+//             if (user === null || user.password === null) {
+//                 return null;
+//             }
+
+//             const result = compareCredentials(credentials.password, user.password);
+
+//             if (!result) {
+//                 return null;
+//             }
+//             return user;
+//         }
+//     })
+// ],
+// callbacks: {
+//     jwt: async ({ token, user }) => {
+//         if (user) {
+//             token.id = user.id;
+//         }
+//         return token;
+//     },
+//     session: ({ session, token }) => {
+//         // if (token && session.id! !== undefined) {
+//         //     session.id = token.id;
+//         // }
+//         return session;
+//     }
+// },
+// secret: 'test',
+// session: {
+//     strategy: 'jwt'
+// },
+// jwt: {
+//     secret: 'test',
+//     encryption: true
+// } as Partial<JWTOptions>
+// });
