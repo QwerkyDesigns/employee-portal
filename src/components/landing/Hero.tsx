d9d386b4426de662866be991ea1c4255ebe6c344
Paul Gradie <paul.e.gradie@gmail.com>
2023-04-29 13:22:37 +1000

    link to login

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -23,7 +23,7 @@ export function Hero() {
                 Generating the artwork is the hard part. We&apos;ll help you with that.
             </p>
             <div className="mt-10 flex justify-center gap-x-6">
-                <Button href="/register" variant={'solid'} color={'slate'} className={''}>
+                <Button href="/login" variant={'solid'} color={'slate'} className={''}>
                     Get 500,000 free credits to start! lmao
                 </Button>
             </div>
